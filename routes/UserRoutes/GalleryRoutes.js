00fd4adeaa0e39f4cfdd47c1ed5cfc099e2350d1
clifton <clifton.lobo@mahity.com>
2026-02-28 18:06:31 +0530

    remove auth from gallery1

--- routes/UserRoutes/GalleryRoutes.js
+++ routes/UserRoutes/GalleryRoutes.js
@@ -10,12 +10,13 @@ const { upload }         = require("../../helpers/Cloudinary");
 const { authMiddleware }  = require("../../controllers/auth/authController");
 
 
-router.use(authMiddleware)
 router.get("/albums",          getApprovedAlbums);
 router.get("/albums/:id/photos", getAlbumPhotos);
 
-router.post("/albums",         upload.array("photos", 50), submitAlbum);
-router.get( "/my-albums",      getMyAlbums);
+
+
+router.post("/albums",        authMiddleware, upload.array("photos", 50), submitAlbum);
+router.get( "/my-albums",  authMiddleware,    getMyAlbums);
 
 module.exports = router;
 
